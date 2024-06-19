574e5a38d388a16d8eee7aa57f12e74bec183a10
vinod_sigadana_x <vinod_sigadana_x@telkomsel.co.id>
2024-06-19 18:32:42 +0530

    Update definition file

--- packages/when/src/index.d.ts
+++ packages/when/src/index.d.ts
@@ -1,2 +1,14 @@
 export type callbackFn = (...args: any[]) => void
-export type when = (condition: boolean, truthyFn: callbackFn, falsyFn: callbackFn) => void;
+/**
+ Detect whether the terminal supports Unicode.
+
+ @example
+ ```
+ import when = require('sv-when');
+
+ when(condition, truthyFn, falsyFn);
+ ```
+ */
+declare function when(condition: boolean, truthyFn: callbackFn, falsyFn: callbackFn): void;
+
+export default when;
