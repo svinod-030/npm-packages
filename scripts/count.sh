61099d832482d0c880c4a29ead818359e88d245b
Vinod Sigadana <vinodsigadana@Vinods-MacBook-Pro.local>
2025-04-04 17:01:00 +0530

    Add scripts

---
+++ scripts/count.sh
@@ -0,0 +1,19 @@
+echo "======================="
+echo "1. feature-toggle-js"
+echo "last month"
+curl https://api.npmjs.org/downloads/point/last-month/feature-toggle-js
+echo "\nlast week"
+curl https://api.npmjs.org/downloads/point/last-week/feature-toggle-js
+echo "\nlast day"
+curl https://api.npmjs.org/downloads/point/last-day/feature-toggle-js
+
+echo "\n=======================\n"
+
+echo "2. workflow-composer"
+echo "last month"
+curl https://api.npmjs.org/downloads/point/last-month/workflow-composer
+echo "\nlast week"
+curl https://api.npmjs.org/downloads/point/last-week/workflow-composer
+echo "\nlast day"
+curl https://api.npmjs.org/downloads/point/last-day/workflow-composer
+echo "\n======================="
